3203bd04145f7cb16e0994b71b9793450b047c66
Taku24 <taku178.24@hotmail.co.jp>
2017-08-07 21:12:39 +0900

    create ARActivity

---
+++ app/src/main/java/docomohackathonar/taku/com/docomohackathonar/Screens/ShowARActivity.java
@@ -0,0 +1,19 @@
+package docomohackathonar.taku.com.docomohackathonar.Screens;
+
+import android.os.Bundle;
+
+import docomohackathonar.taku.com.docomohackathonar.R;
+import eu.kudan.kudan.ARActivity;
+
+/**
+ * Created by taku24 on 2017/08/07.
+ */
+
+public class ShowARActivity extends ARActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_ar);
+    }
+}
